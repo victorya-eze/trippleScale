012ea9e5e3ad15e91ada1199ab4a36455c772698
Victory Eze <161460976+victorya-eze@users.noreply.github.com>
2025-06-06 00:24:33 +0100

    chore: add missing newline at EOF

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js']
+  testMatch: ['**/__tests__/**/*.test.js?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(png|svg|jpg|jpeg|gif)$': '<rootDir>/__mocks__/fileMock.js'
+  }
 };
